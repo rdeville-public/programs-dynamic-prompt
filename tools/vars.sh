0c9d03aedcb57cc446cfb7ff9c9d0ab40c63bfed
Romain Deville <code@romaindeville.fr>
2021-12-02 17:21:04 +0100

    ✨👥 Add tools scripts and contributors

---
+++ tools/vars.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# SHELLCHECK
+# =============================================================================
+# Globally disable some shellcheck warning
+# shellcheck disable=SC2034
+# SC2034: WINDOWS_ID appears unused. Verify it or export it.
+
+WINDOWS_ID=(
+  "0x1600003" ""
+)
